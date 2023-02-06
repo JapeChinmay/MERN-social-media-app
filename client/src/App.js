edf23c7811111ac7510d359c2be59eb2e4dfc56c
Chinmay Jape <iam95041@gmail.com>
2023-02-06 13:10:04 +0530

    added mui, redux , etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,34 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import LoginPage from "Components/LoginPage/LoginPage";
+import ProfilePage from "Components/ProfilePage/ProfilePage";
+import HomePage from "Components/HomePage/homepage";
+import LoginPage from "Components/LoginPage/LoginPage";
+import { themeSettings } from "theme";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/system";
+
 function App() {
-  return <div></div>;
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
